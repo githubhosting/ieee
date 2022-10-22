046d9a4497c1fa5bc26b3c2bfd6ca1cc7f7128ec
Amith M <amitharun3@gmail.com>
2022-10-22 18:36:21 +0530

    Update members.jsx

--- src/pages/members.jsx
+++ src/pages/members.jsx
@@ -12,75 +12,51 @@ import img2 from '@/images/members/megha.png'
 import img3 from '@/images/members/vijayakumar.png'
 import img4 from '@/images/members/sumana.png'
 import img5 from '@/images/members/kishore.png'
-import { Fragment, useState } from 'react'
-import {
-  Accordion,
-  AccordionHeader,
-  AccordionBody,
-} from '@material-tailwind/react'
+
 const members = [
   {
-    id: 1,
     name: 'Dr. Vijaya Kumar B. P',
     designation: 'Advisor',
+    description:
+      'Dr. Vijay Kumar is a Professor with research experience of 20+ Yrs in the areas of Cognitive and Mobile Computing. Technical Advisor for Heart Rescue India & Road Traffic Injuries, National Task force project, Advisor, IEEE CIS, Bangalore section.',
     image: img3,
     profile: '/documents/Vijaya Kumar-profile.pdf',
-    details: "detailed member's profile",
   },
   {
-    id: 2,
     name: 'Dr. Megha. P. Arakeri',
     designation: 'Chair',
+    description:
+      'Dr. Megha Arakeri has a Ph.D from NITK, Surathkal. Working as Associate Professor in MSRIT with 18 years of teaching and research experience in the areas of Computer Vision.',
     image: img2,
     profile: '/documents/Megha-profile.pdf',
-    details: "detailed member's profile",
   },
   {
-    id: 3,
     name: 'Mr. A. T. Kishore',
     designation: 'Vice Chair',
+    description:
+      'Flight lieutenant AT Kishore is currently serving as an  COO of Paramount Exhibitors. He is interested in subjects related to Optical Fiber Technologies, UAV Industries and  CDMA technologies. He is senior professional member of IEEE and on the smart city advisory committee of IEEE',
     image: img5,
     profile: '/documents/Kishore-profile.pdf',
   },
   {
-    id: 4,
     name: 'Dr. Vinutha. D.C',
     designation: 'Secretary',
+    description:
+      'Dr. Vinutha DC is a Ph. D degree holder from VTU and is currently serving as an  professor and head in CSE AI/ML at VVCE. She is interested in subjects related to Data Analytics, Machine Learning, Big Data and Computer Networks',
     image: img1,
     profile: '/documents/Vinutha-profile.pdf',
   },
   {
-    id: 5,
     name: 'Dr. Sumana. M',
     designation: 'Treasurer',
+    description:
+      'Dr. Sumana Maradithaya is a Ph. D degree holder from Manipal University and is currently serving as an associate professor in IS&E department of MSRIT. She is interested in subjects related to Data Mining , Data Science and Analytics, Machine Learning and Deep Learning',
     image: img4,
     profile: '/documents/Sumana-profile.pdf',
   },
 ]
-function Icon({ id, open }) {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      className={`${
-        id === open ? 'rotate-180' : ''
-      } h-5 w-5 transition-transform`}
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke="currentColor"
-      strokeWidth={2}
-    >
-      <path strokeLinecap="round" strokeLinejoin="round" d="M19 9l-7 7-7-7" />
-    </svg>
-  )
-}
 
 export default function Login() {
-  const [open, setOpen] = useState(0)
-
-  const handleOpen = (value) => {
-    setOpen(open === value ? 0 : value)
-  }
-
   return (
     <>
       <Head>
@@ -89,65 +65,47 @@ export default function Login() {
         </title>
       </Head>
       <Header />
-      <section class="body-font text-gray-600">
-        <div class="container mx-auto px-5 py-12">
-          <div class="mb-20 flex w-full flex-col text-center">
-            <h1 class="title-font mb-4 text-2xl font-medium text-gray-900">
-              Office Bearers
-            </h1>
-            <p class="mx-auto text-base leading-relaxed lg:w-2/3">
-              IEEE Computational Intelligence Society, Bangalore
-            </p>
-          </div>
-          <div class="-m-4 flex flex-wrap justify-center gap-2.5 md:gap-3 lg:gap-5">
+      <section class="bg-white">
+        <div class="container mx-auto px-6 py-10">
+          <h1 class="text-center text-3xl font-semibold capitalize text-gray-800">
+            Our <span class="text-blue-500">Executive Team</span>
+          </h1>
+
+          <p class="mx-auto my-6 max-w-2xl text-center text-gray-500"></p>
+
+          <div class="mt-8 grid grid-cols-1 gap-8 md:grid-cols-2 xl:mt-16 xl:grid-cols-2">
             {members.map((member) => (
-              <div class="rounded-xl border px-14 py-6 shadow-lg sm:w-full md:w-1/2 lg:w-1/4">
-                <div class="flex h-full flex-col items-center text-center">
-                  <div alt="team">
+              <div class="group transform cursor-pointer rounded-xl border px-12 py-8 transition-colors duration-300">
+                <div class="flex flex-col sm:-mx-4 sm:flex-row">
+                  <div>
                     <Image
-                      class="mb-4 h-48 w-48 flex-shrink-0 rounded-lg object-cover object-center sm:mb-0"
+                      class="flex-shrink-0 rounded-lg object-cover  sm:mx-4"
                       src={member.image}
-                      height={192}
-                      width={192}
+                      alt=""
+                      width="270px"
+                      height="270px"
                     />
                   </div>
-                  <div class="w-full">
-                    <h2 class="title-font text-lg font-medium text-gray-900">
+
+                  <div class="mt-4 sm:mx-4 sm:mt-0">
+                    <h1 class="text-xl font-semibold capitalize text-gray-700 group-hover:text-white ">
                       {member.name}
-                    </h2>
-                    <h3 class="mb-3 text-gray-500">{member.designation}</h3>
-                    {/* <a
-                      href={member.profile}
-                      target="_blank"
-                      rel="noreferrer"
-                      class="inline-flex items-center justify-center py-2 px-4 text-center text-sky-500 lg:px-8 xl:px-10"
-                    >
-                      View Profile
-                      <svg
-                        fill="none"
-                        stroke="currentColor"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        class="ml-2 h-4 w-4"
-                        viewBox="0 0 24 24"
-                      >
-                        <path d="M5 12h14M12 5l7 7-7 7"></path>
-                      </svg>
-                    </a> */}
-                    <Fragment>
-                      <Accordion
-                        open={open === 2}
-                        icon={<Icon id={2} open={open} />}
-                      >
-                        <AccordionHeader onClick={() => handleOpen(2)}>
-                          View Profile
-                        </AccordionHeader>
-                        <AccordionBody>{member.details}</AccordionBody>
-                      </Accordion>
-                    </Fragment>
+                    </h1>
+
+                    <p class="mt-2 capitalize text-gray-500 group-hover:text-gray-300 ">
+                      {member.designation}
+                    </p>
                   </div>
                 </div>
+
+                <p class="mt-4 capitalize text-gray-500 group-hover:text-gray-300 ">
+                  {member.description}
+                </p>
+
+                <a class="group inline-flex items-center justify-center rounded-full py-2 px-4 text-sm font-semibold focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 bg-slate-900 text-white hover:bg-slate-700 hover:text-slate-100 active:bg-slate-800 active:text-slate-300 focus-visible:outline-slate-900 text-md mt-10 rounded bg-sky-600 px-4 py-1.5 font-semibold text-gray-50"
+                    href={member.profile}>
+                  View Profile
+                </a>
               </div>
             ))}
           </div>
